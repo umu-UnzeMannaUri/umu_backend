9aea667e32c71771286f6ee3bab9fa16403040a8
김경태 <162432441+rudwk@users.noreply.github.com>
2026-08-19 16:26:03 +0900

    Delete umu_backend directory

--- umu_backend/src/common/decorators/current-user.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const CurrentUser = createParamDecorator((_: unknown, ctx: ExecutionContext): string => {
-  const request = ctx.switchToHttp().getRequest();
-  return request.userId;
-});
